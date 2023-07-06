4b8a0e7cb41b44f8cc70454fb47fa154db805bce
sebasxm00 <sebasmm2304@gmail.com>
2023-07-06 14:09:32 -0500

    Working version

--- script.py
+++ script.py
@@ -1,70 +1,112 @@
-available_cells = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
-played_cells = {}
-table = """
-    A   B   C
+class TicTacToe:
+    def __init__(self):
+        self.available_cells = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
+        self.players = []
+        self.table = """
+            A   B   C
 
-1   A1  B1  C1
+        1   A1  B1  C1
 
-2   A2  B2  C3
+        2   A2  B2  C3
 
-3   A3  B3  C3"""
-winner = ''
+        3   A3  B3  C3"""
+        self.winner = ''
+        self.last_played = ''
+        self.combinations_to_win = [
+            ['A1', 'A2', 'A3'],
+            ['B1', 'B2', 'B3'],
+            ['C1', 'C2', 'C3'],
+            ['A1', 'B1', 'C1'],
+            ['A2', 'B2', 'C2'],
+            ['A3', 'B3', 'C3'],
+            ['A1', 'B2', 'C3'],
+            ['A3', 'B2', 'C1']
+        ]
 
-def draw_table():
-    draw_table = table
-    for cell in available_cells:
-        draw_table = draw_table.replace(cell, '')
-    print(draw_table)
+    def start_game(self, phase = 0):
+        self.available_cells = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
+        self.table = """
+            A   B   C
 
-def play_round(name):
-    global table
-    print(name + " it's your turn")
-    player_cell = input("Select one cell (Ex: A1, B2, C3): ")
-    played_cells[name].append(available_cells.pop(available_cells.index(player_cell)))
-    if name == player1_name:
-        table = table.replace(player_cell, 'O')
-    else:
-        table = table.replace(player_cell, 'X')
+        1   A1  B1  C1
 
-def check_win():
-    global winner
-    combinations_to_win = [
-        ['A1', 'A2', 'A3'],  # Filas
-        ['B1', 'B2', 'B3'],
-        ['C1', 'C2', 'C3'],
-        ['A1', 'B1', 'C1'],  # Columnas
-        ['A2', 'B2', 'C2'],
-        ['A3', 'B3', 'C3'],
-        ['A1', 'B2', 'C3'],  # Diagonales
-        ['A3', 'B2', 'C1']
-    ]
+        2   A2  B2  C3
 
-    for player_name, cells in played_cells.items():
-        for combination in combinations_to_win:
-            if all(cell in cells for cell in combination):
-                winner = player_name
-                return True
+        3   A3  B3  C3"""
+        self.winner = ''
+        self.last_played = ''
+        if phase == 0:
+            self.players = []
+            print("Welcome to Sebas TicTacToe made in Python 3, hope you enjoy while playing!")
+            print("Let's begin by setting your names")
+            player1 = Player(input("Player1's name: "), self)
+            player2 = Player(input("Player2's name: "), self)
+            self.players.extend([player1, player2])
+            print("Let's start!")
+            self.start_game(1)
+        if phase == 1:
+            player1 = self.get_player(1)
+            player2 = self.get_player(2)
+            while self.check_winner() == False:
+                self.draw_table()
+                if self.last_played == self.get_player(1):
+                    player2.play_round()
+                else:
+                    player1.play_round()
+            self.draw_table()
+            print("We have a winner!")
+            print("Congratulations on winning {winner_name}".format(winner_name=self.winner))
+            play_again = input("Do you wish to play again? (Y/N) ")
+            if play_again == 'Y':
+                for player in self.players:
+                    player.played_cells = []
+                self.start_game(1)
+            else:
+                print("Okay! Have a great day.")
 
-    return False
+    def get_player(self, index):
+        return self.players[index - 1]
 
+    def draw_table(self):
+        table_to_draw = self.table
+        for cell in self.available_cells:
+            table_to_draw = table_to_draw.replace(cell, ' ')
+        print(table_to_draw)
 
-print("Welcome to Sebas TicTacToe made in Python 3, hope you enjoy while playing!")
-print("Let's begin by setting your names")
-player1_name = input("Player1's name: ")
-player2_name = input("Player2's name: ")
-played_cells.update({player1_name:[], player2_name:[]})
-print("Great! Let's start")
+    def check_winner(self):
+        for player in self.players:
+            for combination in self.combinations_to_win:
+                if all(cell in player.played_cells for cell in combination):
+                    self.winner = player.name
+                    return True
+        return False
+    
+class Player:
+    def __init__(self, name, game):
+        self.name = name
+        self.game = game
+        self.played_cells = []
 
-last_played = ''
+    def is_o(self):
+        return self == self.game.players[0]
+    
+    def is_x(self):
+        return self == self.game.players[1]
 
-while(check_win() == False):
-    draw_table()
-    if last_played == player1_name:
-        play_round(player2_name)
-        last_played = player2_name
-    else:
-        play_round(player1_name)
-        last_played = player1_name
+    def play_round(self):
+        print("{player_name} it's your turn".format(player_name=self.name))
+        selected_cell = input("Select one cell (Ex: A1, B2, C3): ")
+        while selected_cell not in self.game.available_cells:
+            print("Please select a valid cell")
+            selected_cell = input("Select one cell (Ex: A1, B2, C3): ")
+        self.played_cells.append(selected_cell)
+        self.game.available_cells.remove(selected_cell)
+        if self.is_o():
+            self.game.table = self.game.table.replace(selected_cell, 'O ')
+        elif self.is_x():
+            self.game.table = self.game.table.replace(selected_cell, 'X ')
+        self.game.last_played = self
 
-draw_table()
-print("Winner: " + winner)
+game = TicTacToe()
+
+game.start_game()
